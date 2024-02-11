3bc971134a764dff39c51b6ffc5771830fe11846
Tatsuya Kosuge <castero1219@gmail.com>
2024-02-12 00:38:11 +0900

    [modify]id名修正

--- day1/part2-jspractice/js/part2.js
+++ day1/part2-jspractice/js/part2.js
@@ -1,11 +1,14 @@
-$("button").on("click", function () {
+$("#btn").on("click", function () {
   const season = $("#season-select").val();
   let votesCount = $(`#${season}-count`).html();
   votesCount = parseInt(votesCount) + 1;
   $(`#${season}-count`).html(votesCount);
-  $(`#${season}-gage`).css({
-    width: `${votesCount * 10}px`,
-  });
+  $(`#${season}-gage`).animate(
+    {
+      width: `${votesCount * 10}px`,
+    },
+    500
+  );
 
   const comment = $("#vote-comment").val();
   $(`#option-${season}`).append(`<div> ${comment}</div>`);
