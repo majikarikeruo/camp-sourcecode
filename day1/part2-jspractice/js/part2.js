24e956702566ace37b8ebc9bd8460621f87d39d5
Tatsuya Kosuge <castero1219@gmail.com>
2024-08-14 01:14:11 +0900

    [mdify]テンプレート変更

--- day1/part2-jspractice/js/part2.js
+++ day1/part2-jspractice/js/part2.js
@@ -13,14 +13,24 @@ console.log(46); //数値の1が表示
 console.log("日向坂"); //文字の1が表示
 console.log("日向坂46"); //こんにちはいう文字が表示
 
-let kinokoCount = 0;
+let count = 0; // 0票からスタート。変数countの目的は「現在の票数」を記憶させる場所。
+
+//clickされたときに行いたいプログラムを実行する
 $("#btn-kinoko").on("click", function () {
-  kinokoCount = kinokoCount + 1;
-  $(`#count-kinoko`).html(kinokoCount);
+  // 現在の票数を確認し、確認したデータに1を足す
+  // 1を足したら、現在の票数は「０->1」に変わるので、現在の状態を再度記憶させる
+  // 頭にletやconstがない場合、元々用意されている変数の中身を上書きすることができる
+  count = count + 1;
+
+  $(`#count-kinoko`).html(`${count}票`); // 変数名を()の中に指定すれば、記憶させておいた情報を指定した場所に表示させたりすることができます！
 });
 
-$("#btn-takenoko").on("click", function () {
-  let votesCount = parseInt($(`#count-takenoko`).html()) + 1;
-  $(`#count-takenoko`).html(votesCount);
-  $("#gage-takenoko").animate({ height: `${10 * votesCount}px` }, 500);
-});
+// let takenokoCount = 0;
+// $("#btn-takenoko").on("click", function () {
+//   // 現在の票数を確認し、確認したデータに1を足す
+//   // 1を足したら、現在の票数は「０->1」に変わるので、現在の状態を再度記憶させる
+//   // 頭にletやconstがない場合、元々用意されている変数の中身を上書きすることができる
+//   takenokoCount = takenokoCount + 1;
+
+//   $(`#count-takenoko`).html(`${takenokoCount}票`); // 変数名を()の中に指定すれば、記憶させておいた情報を指定した場所に表示させたりすることができます！
+// });
