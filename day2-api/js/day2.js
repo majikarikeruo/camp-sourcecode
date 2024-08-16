4b4e7ed2e9a3ce76bda5269135d2f67c36089076
majikarikeruo <castero1219@gmail.com>
2024-08-16 11:25:55 +0900

    [modify]配列のところforに変更

--- day2-api/js/day2.js
+++ day2-api/js/day2.js
@@ -5,25 +5,23 @@ $("#btn").on("click", function () {
   $.getJSON(requestUrl, function (data) {
     //　情報を取得できた後にやりたいことをここに書く
     console.log(data);
+
     const randomNum = Math.floor(Math.random() * data.results.length);
     const targetSong = data.results[randomNum].previewUrl;
     $("#audio").attr("src", targetSong);
-    $("#audio")[0].volume = 0.1; // volumeは任意
+    $("#audio")[0].volume = 0.1; // volumeは任意。
     $("#audio")[0].play();
 
     // 以下は一応入れてますが、やるとしてもたぶんコピペで駆け抜けになると思われるしやらないかも
-    data.results.forEach(function (result) {
-      const trackName = result.trackName;
-      const artistName = result.artistName;
-      const artworkUrl = result.artworkUrl100;
+    for (let i = 0; i < data.results.length; i++) {
       const music = `<div class="music">
-        <img src="${artworkUrl}" alt="${trackName}">
+        <img src="${data.results[i].artworkUrl100}" alt="${data.results[i].trackName}">
         <div class="music-content">
-          <p class="track-name">${trackName}</p>
-          <p class="artist-name">${artistName}</p>
+          <p class="track-name">${data.results[i].trackName}</p>
+          <p class="artist-name">${data.results[i].artistName}</p>
         </div>
       </div>`;
       $("#result").append(music);
-    });
+    }
   });
 });
